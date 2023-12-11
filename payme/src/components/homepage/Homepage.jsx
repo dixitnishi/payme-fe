df80c5cfa57c87b9780f2b24082128b0dc875e6d
dixitnishi <dixit.nishi0409@gmail.com>
2023-12-11 21:55:42 +0530

    Added tailwind dependencies

---
+++ payme/src/components/homepage/Homepage.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../navbar/Navbar";
+
+
+
+const Homepage = () =>{
+    
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    );
+
+
+}
+
+export default Homepage;
